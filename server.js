1d186aa6ced1f584d465567056169f8c3e26b745
sabinn12 <irakizasabin12@gmail.com>
2024-03-09 18:12:54 +0200

    updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Product = require('./models/productModel')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //routes
 app.get('/', (req,res) => {
@@ -56,6 +57,19 @@ app.put('/products/:id', async(req,res) =>{
         }
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+// delete a product
+app.delete('/products/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({messsage: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
     } catch (error) {
         res.status(500).json({message: error.message})
     }
