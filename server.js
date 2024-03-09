7cd8e9f92eafb6b68a2ace7d1dd9fed4c3b0ffed
sabinn12 <irakizasabin12@gmail.com>
2024-03-09 17:39:06 +0200

    updates

--- server.js
+++ server.js
@@ -1,14 +1,59 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
-
+app.use(express.json())
 
 //routes
-
-app.get('/', (req, res) => {
-    res.send('Hello Node Api')
+app.get('/', (req,res) => {
+    res.send('Hello Node API')
 })
 
-app.listen(3000, ()=>{
-    console.log("Node Api app is running on port 3000")
+app.get('/blog', (req,res) => {
+    res.send('Hello blogs , my name is irakiza sabin')
+})
+
+app.get('/products', async(req,res) =>{
+    try{
+       const products = await Product.find({});
+       res.status(200).json(products);
+    }catch (error){
+        res.status(500).json({message:error.message})
+    }
+}) 
+
+app.get('/products/:id',async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+
+app.post('/products', async(req,res) => {
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+
+    }catch (error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://irakizasabin12:irakizasabin@cluster0.hqk7hht.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, () => {
+        console.log('Node api app is running on port 3000')
+    })
+  
+}).catch(() =>{
+    console.log(error)
 })
